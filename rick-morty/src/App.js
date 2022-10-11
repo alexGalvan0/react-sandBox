4a78f47b720574da6171ab4f85d46b14569aba23
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-11 20:40:18 +0000

    added data to app

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,21 +1,28 @@
 import Nav from './componenets/Nav';
 import ActionAreaCard from './componenets/ActionAreaCard'
-import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
 function App() {
+  const [data, setData] = useState([])
+  const url = "https://rickandmortyapi.com/api/character";
+  
+  useEffect(() => {
+    axios.get(url)
+      .then((resp) => {
+        setData(resp.data.results);
+      })
+  }, []);
+
   return (
     <div className="App container-fluid">
       <Nav />
-
-      <Button variant='contained' color='secondary'>CLICK ME</Button>
-      <div className='row'>
-        <div className='col pt-5 d-flex justify-content-center'>
-
-          <ActionAreaCard  />
-        </div>
-      </div>
+      <Grid container justifyContent="center" spacing={2}>
+        {data.map((c) => <ActionAreaCard key={c.name} character={c} />)}
+      </Grid>
     </div>
   );
 }
--- rick-morty/src/componenets/ActionAreaCard.jsx
+++ rick-morty/src/componenets/ActionAreaCard.jsx
@@ -1,58 +1,45 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Grid } from "@mui/material";
 
-import axios from 'axios'
+import axios from "axios";
 
-export default function ActionAreaCard() {
-  const [ char, setChar ] = useState('');
-  const [ num, setNum ] = useState(0);
-  const [ image, setImage ] = useState('');
-  const [ response, setResponse ] = useState('');
+export default function ActionAreaCard({ character }) {
+  // const [char, setChar] = useState("");
+  // const [num, setNum] = useState(0);
+  // const [image, setImage] = useState("");
 
-
-  const url = 'https://rickandmortyapi.com/api/character'
-
-
-  const getChat = async () => {
-    const response = await axios.get(url)
-
-    setChar(response.data.results[num].name)
-    setImage(response.data.results[num].image)
-    setNum(num<19 ? num+1: 0)
-
-  }
-
-  useEffect(()=>{
-    getChat()
-  },[])
-
-
-
+  // const getChat = () => {
+  //   setChar(data[num].name);
+  //   setImage(data[num].image);
+  //   setNum(num < 19 ? num + 1 : 0);
+  // };
 
   return (
-    <Card sx={{ maxWidth: 345 }} onClick={getChat}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image={image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {char}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Grid item>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={character.image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {character.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
   );
 }
