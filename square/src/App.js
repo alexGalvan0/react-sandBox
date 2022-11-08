7568995e385d55b48eeb7315d1f75ba58e324a0d
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-08 03:02:09 +0000

    square

---
+++ square/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MyPaymentForm from './componenets/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <MyPaymentForm />
+
+    </div>
+  );
+}
+
+export default App;
